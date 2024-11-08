9a610f61bef74c8cf4a6cdf7d987d9db1b381909
thanhtuandev <pro03tuan@gmail.com>
2024-11-08 15:32:03 +0700

    'luu'

---
+++ test.c
@@ -0,0 +1,21 @@
+// #include <stdio.h>
+// #include <stdlib.h> // Cần thêm thư viện này để dùng system("pause")
+
+// int main() {
+//     int age;
+
+//     printf("Nhap tuoi cua ban: ");
+//     scanf("%d", &age);  // Nhập giá trị cho biến 'age'
+    
+//     printf("Tuoi cua ban la: %d\n", age);  // In giá trị của biến 'age'
+    
+//     system("pause"); // Tạm dừng chương trình cho đến khi người dùng nhấn một phím
+    
+//     return 0;
+// }
+
+#include <stdio.h>  
+int main() {  
+   printf("Hello C Programming\n");  
+   return 0;  
+}
